a45045d6c77b06def761d7ebd0b9767fd9350384
Marcus Wallstersson <wallstersson@hotmail.com>
2011-09-15 17:54:37 +0200

    Added some code

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,28 @@
 
-public class Customer {
+public class Customer
+{
+
+	private int enterShop;
+	private int queueTime;
+
+	public int getEnterShop()
+	{
+		return enterShop;
+	}
+
+	public void setEnterShop(int enterShop)
+	{
+		this.enterShop = enterShop;
+	}
+
+	public int getQueueTime()
+	{
+		return queueTime;
+	}
+
+	public void setQueueTime(int queueTime)
+	{
+		this.queueTime = queueTime;
+	}
 
 }
---
+++ src/Service.java
@@ -0,0 +1,27 @@
+
+public class Service
+{	
+	private double serviceTime;
+	private boolean working;
+	
+	public double getServiceTime()
+	{
+		return serviceTime;
+	}
+	
+	public void setServiceTime(double serviceTime)
+	{
+		this.serviceTime = serviceTime;
+	}
+	
+	public boolean isWorking()
+	{
+		return working;
+	}
+	
+	public void setWorking(boolean working) 
+	{
+		this.working = working;
+	}
+	
+}
