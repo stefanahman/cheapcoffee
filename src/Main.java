1f863448d2e64eb9852b4c12eca574f2f057c4fa
Marcus Wallstersson <wallstersson@hotmail.com>
2011-09-18 21:00:16 +0200

    fixed output

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,12 @@ public class Main
 	 	seed = (long)Integer.parseInt(args[0]);
 	 	replications = Integer.parseInt(args[1]);
 	 	maxQueueSize = Integer.parseInt(args[2]);
-	 	modelTime = Integer.parseInt(args[3]);
+	 	if(args.length == 4) {
+		 	modelTime = Integer.parseInt(args[3]);
+	 	}
+	 	else {
+	 		modelTime = 720;
+	 	}
 		
 		System.out.println("#################################################");
 		System.out.println("#						#");
@@ -32,32 +37,46 @@ public class Main
 		System.out.println("# Seed:					" + seed + "	#");
 		System.out.println("# Replications:				" + replications + "	#");
 		System.out.println("# Queue:				" + maxQueueSize + "	#");
-		System.out.println("# Simulation length:			" + modelTime + "	#");
+		System.out.println("# Replication length:			" + modelTime + " min	#");
 		
 		simulation = new CheapCoffee[replications];
 		
 		rnd.setSeed(seed);
 		
 		for (int i = 0; i < replications; i++) {
-//			System.out.println("#################################################");
-//			System.out.println("#						#");
-//			System.out.println("# Replication " + (i + 1) + "					#");
-//			System.out.println("#						#");
-//			System.out.println("#################################################");
 			long rndSeed = rnd.nextLong();
 			simulation[i] = new CheapCoffee(rndSeed, modelTime);
 			sumAverageQueueTime += simulation[i].getAverageQueueTime();
 			sumPercentRejected += simulation[i].getPercentRejected();
 			sumTotalCustomers += simulation[i].getTotalCustomers();
+			System.out.println("#########################################################");
+			System.out.println("#							#");
+			System.out.println("# Replication " + (i + 1) + "					#");
+			System.out.println("#							#");
+			System.out.println("#########################################################");
+			System.out.println("#							#");
+			System.out.println("# Total customers:			" + simulation[i].getTotalCustomers() + "		#");
+			System.out.println("# Rejected customers:			" + String.format("%.5g",100*simulation[i].getPercentRejected()) + "%		#");
+			System.out.println("# Average queueing time:		" + String.format("%.5g",simulation[i].getAverageQueueTime()) + " min	#");
+			System.out.println("#########################################################");
+			System.out.println("");
+
+
+
 		}
-		System.out.println("#################################################");
-		System.out.println("#						#");
-		System.out.println("# Summary					#");
-		System.out.println("#						#");
-		System.out.println("#################################################");
-		System.out.println("# Total average average queue time:	" + String.format("%.5g",(double) sumAverageQueueTime/replications) + "	#");
-		System.out.println("# Total average percent rejected:	" +  String.format("%.5g",(double) 100*sumPercentRejected/replications) + "%	#");
-		System.out.println("# Total average customers: 		" + (double) sumTotalCustomers/replications + "	#");
-		System.out.println("#################################################");
+		System.out.println("#########################################################");
+		System.out.println("#							#");
+		System.out.println("# Average of: "+ replications +" replications with			#");
+		System.out.println("#							#");
+		System.out.println("# Seed:						" + seed + "	#");
+		System.out.println("# Replications:					" + replications + "	#");
+		System.out.println("# Queue:					" + maxQueueSize + "	#");
+		System.out.println("# Replication length:				" + modelTime + " min	#");
+		System.out.println("#							#");
+		System.out.println("#########################################################");
+		System.out.println("# Total average customers: 		" + String.format("%.5g",(double) sumTotalCustomers/replications) + "		#");
+		System.out.println("# Total average percent rejected:	" +  String.format("%.5g",(double) 100*sumPercentRejected/replications) + "%		#");
+		System.out.println("# Total average average queue time:	" + String.format("%.5g",(double) sumAverageQueueTime/replications) + " min	#");
+		System.out.println("#########################################################");
 	}
 }
